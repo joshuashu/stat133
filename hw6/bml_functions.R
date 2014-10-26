d1b02fd4b1206c17eed57c78a2718b086b050ca0
Karl Kumbier <kkumbier@berkeley.edu>
2014-10-26 14:53:16 -0700

    add hw6

---
+++ hw6/bml_functions.R
@@ -0,0 +1,36 @@
+#################################################################################
+#### Functions for BML Simulation Study
+
+
+#### Initialization function.
+## Input : size of grid [r and c] and density [p]
+## Output : A matrix [m] with entries 0 (no cars) 1 (red cars) or 2 (blue cars)
+## that stores the state of the system (i.e. location of red and blue cars)
+
+bml.init <- function(r, c, p){
+
+  
+   #return(m)
+}
+
+#### Function to move the system one step (east and north)
+## Input : a matrix [m] of the same type as the output from bml.init()
+## Output : TWO variables, the updated [m] and a logical variable
+## [grid.new] which should be TRUE if the system changed, FALSE otherwise.
+
+## NOTE : the function should move the red cars once and the blue cars once,
+## you can write extra functions that do just a step north or just a step east.
+
+bml.step <- function(m){
+
+  
+   #return(list(m, grid.new))
+}
+
+#### Function to do a simulation for a given set of input parameters
+## Input : size of grid [r and c] and density [p]
+## Output : *up to you* (e.g. number of steps taken, did you hit gridlock, ...)
+
+bml.sim <- function(r, c, p){
+
+}
---
+++ hw6/bml_simulation.R
@@ -0,0 +1,10 @@
+#################################################################################
+#### BML Simulation Study
+
+#### Put in this file the code to run the BML simulation study for a set of input parameters.
+#### Save some of the output data into an R object and use save() to save it to disk for reference
+#### when you write up your results.
+#### The output can e.g. be how many steps the system took until it hit gridlock or
+#### how many steps you observered before concluding that it is in a free flowing state.
+
+
